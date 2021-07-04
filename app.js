267792ac4334fc0c5f4a0ceb3d7cf6b8003db1c7
JulianaLezcano <juliana.v.lezcano@hotmail.com>
2021-07-03 21:38:48 -0300

    m

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let port = 7000;
 
 app.use(express.static('public'));
 
-app.listen(port,()=> console.log(`Port runnig in ${port}\nAbrir en el navegador http://localhost:${port}`));
+app.listen(port,()=> console.log(`Port running in ${port}\nAbrir en el navegador http://localhost:${port}`));
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/home.html'))
